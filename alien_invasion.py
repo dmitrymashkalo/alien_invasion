2438b31b27546a50a3ffe00330d14d240b3f54ff
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-08 20:57:24 +0200

    Add bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """A class for managing resources and game behavior."""
@@ -18,9 +19,11 @@ class AlienInvasion:
 
         # Window mode
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # Name
         pygame.display.set_caption(self.settings.name)
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         
     def run_game(self):
@@ -28,6 +31,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
 
@@ -52,6 +56,9 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
 
     def _check_keyup_events(self, event):
         """Events handle."""
@@ -60,7 +67,19 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
-                    
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    
+    def _update_bullets(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
 
     def _update_screen(self):
@@ -70,6 +89,10 @@ class AlienInvasion:
 
             # Ship
             self.ship.blitme()
+
+            # Bullet
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
 
             # Update screen
             pygame.display.flip()
--- settings.py
+++ settings.py
@@ -3,10 +3,18 @@ class Settings:
 
     def __init__(self):
         "Init game settings"
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.name = "Alien Invasion"
         self.background_color = (230, 230, 230)
+
+        # Bullet settings
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 60, 60)
+        self.bullet_allowed = 3
 
         # Ship settings
         self.ship_speed = 0.8
