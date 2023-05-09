19700a9a387c96c07a10cdeae9b4565563c8c536
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-10 00:25:49 +0200

    Refactoring alien.py

--- alien.py
+++ alien.py
@@ -3,14 +3,16 @@ from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
+    """ A class representing an alien """
 
     def __init__(self, ai_game):
+        """ Init an alien and set initial position """
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
 
-        # Load image alien and assign attribute rect
-        self.image = pygame.image.load('images/alien.png')
+        # Upload the alien image
+        self.image = pygame.image.load('images/alien_yellow.png')
         self.rect = self.image.get_rect()
 
         # Every new alien appear in left top corner of screen
@@ -22,12 +24,15 @@ class Alien(Sprite):
 
 
     def update(self):
+        """ Moves the alien left or right """
         self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
 
 
     def check_edges(self):
-        """ Return True if an alien in the edge of the screen """
+        """ Check edge of the screen """
         screen_rect = self.screen.get_rect()
+
+        # Return True if an alien touch the edge of the screen
         if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
