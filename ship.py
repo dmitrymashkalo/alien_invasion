4f3c8e37e9c9925926710c09104f1cd335057abc
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-09 23:47:14 +0200

    Refactoring ship.py file

--- ship.py
+++ ship.py
@@ -1,9 +1,11 @@
 import pygame
 
+
 class Ship:
+    """ Ship control class"""
 
     def __init__(self, ai_game):
-        """Init ship and set initial position"""
+        """ Init ship and set initial position """
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -15,21 +17,24 @@ class Ship:
         # Every new ship will appear on mid bottom
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Save coordinats of ship
+        # Save coordinats of ship in self.x as a float
         self.x = float(self.rect.x)
 
         # Moving flags
         self.moving_right = False
         self.moving_left = False
 
+
     def update(self):
+        """ Updates the ship's position based on the flag """
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > self.screen_rect.left:
             self.x -= self.settings.ship_speed
 
         self.rect.x = self.x
-    
+
+
     def blitme(self):
-        """Draw the ship"""
+        """ Draw the ship at the current position """
         self.screen.blit(self.image, self.rect)
