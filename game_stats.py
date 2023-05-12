7dafda850bd974e7ffbbb422e81ea20f988ca3bb
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-12 16:44:04 +0200

    Refactoring: game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -2,30 +2,35 @@ class GameStats:
     """ Class for tracking stats for the game """
 
     def __init__(self, ai_game):
-        """ Init stats """
+        """ Init static game stats """
         self.settings = ai_game.settings
 
-        # Load last record from file if exists
+        # Load last record from file if exists from the txt file
         self.filename = 'record_table.txt'
         self.high_score = self.load_record()
 
+        # Init dynamic game stats
         self.reset_stats()
 
-        # Game starts in active state
+        # Game starts flag
         self.game_active = False
 
 
     def reset_stats(self):
-        """ Init stats that changed during the game """
+        """ Init dynamic game stats """
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.lvl = 1
 
+
     def save_record(self):
+        """ Save high score result in the txt file """
         with open(self.filename, 'w') as file:
             file.write(str(self.high_score))
 
+
     def load_record(self):
+        """ Load existing result from the txt file if exists"""
         try:
             with open(self.filename) as file:
                 self.last_record = file.read()
