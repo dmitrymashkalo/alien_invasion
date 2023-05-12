b3460fa76fd01c4e32f39cdae5e150918d6524cc
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-12 16:38:24 +0200

    Refactoring: score_board.py

--- score_board.py
+++ score_board.py
@@ -14,12 +14,10 @@ class Scoreboard:
         self.settings = ai_game.settings
         self.stats = ai_game.stats
 
-        # Text properties
-        self.text_color = (60, 60, 60)
-        self.bg_color = (255, 255, 255)
+        # Init font
         self.font = pygame.font.SysFont(None, 48)
         
-        # Prepering image
+        # Preparing scoring images
         self.prep_score()
         self.prep_high_score()
         self.prep_lvl()
@@ -28,11 +26,14 @@ class Scoreboard:
     
     def prep_score(self):
         """ Converts the current score to an image """
+        # Rounded the integer
         rounded_score = round(self.stats.score)
+        # Forming string
         score_str = "CURRENT POINTS: {:,}".format(rounded_score)
-        self.score_image = self.font.render(score_str, True, self.text_color, self.bg_color)
+        #Render the string into an image
+        self.score_image = self.font.render(score_str, True, self.settings.text_color, self.settings.background_color)
 
-        # Вывод счета в правой верхней части экрана.
+        # Align the image in the top right part of the screen
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
@@ -40,27 +41,34 @@ class Scoreboard:
     
     def prep_high_score(self):
         """ Converts a high score to an image """
+        # Rounded the integer
         rounded_high_score = round(self.stats.high_score)
+        # Forming a string
         high_score_str = "BEST RESULT: {:,}".format(rounded_high_score)
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.bg_color)
+        # Render the string into an image
+        self.high_score_image = self.font.render(high_score_str, True, self.settings.text_color, self.settings.background_color)
 
-        # Вывод счета в центральной верхней части экрана
+        # Align the image in the top center part of the screen
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = 20
     
 
     def prep_lvl(self):
-        """ Converts a lvl info to an image"""
+        """ Converts a lvl info to an image """
+        # Forming string
         lvl_str = "LEVEL: " + str(self.stats.lvl)
-        self.lvl_image = self.font.render(lvl_str, True, self.text_color, self.bg_color)
+        # Render the string into an image
+        self.lvl_image = self.font.render(lvl_str, True, self.settings.text_color, self.settings.background_color)
 
+        # Align the image under the current scoring
         self.lvl_rect = self.lvl_image.get_rect()
         self.lvl_rect.right = self.score_rect.right
         self.lvl_rect.top = self.score_rect.bottom + 10
 
 
     def prep_health(self):
+        """ Convert health into a group of images """
         self.health = Group()
 
         for health in range(self.stats.ships_left):
@@ -71,7 +79,7 @@ class Scoreboard:
 
 
     def show_score(self):
-        """ Draw score board """
+        """ Draws score board """
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.lvl_image, self.lvl_rect)
@@ -79,6 +87,7 @@ class Scoreboard:
 
 
     def check_high_score(self):
+        """ Compares the current score with the highest and sets the new value in the highest """
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
